d4902781e0de07c7836576177d8345c4c183f513
Atli Sævar Guðmundsson <atliepli@Atli-Svar-Gumundssons-MacBook-Pro.local>
2015-04-05 10:39:51 +0000

    4 pípur og læti

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -10,7 +10,14 @@ window.Game = (function() {
 	var Game = function(el) {
 		this.el = el;
 		this.player = new window.Player(this.el.find('.Player'), this);
-		this.pipe = new window.Pipe(this.el.find('.Pipe'), this);
+		this.pipe1upper = new window.Pipe(this.el.find('.Pipe1Upper'), 90, 0, 10, 6, this);
+		this.pipe1lower = new window.Pipe(this.el.find('.Pipe1Lower'), 90, 30, 30, 6, this);
+		this.pipe2upper = new window.Pipe(this.el.find('.Pipe2Upper'), 130, 0, 20, 6, this);
+		this.pipe2lower = new window.Pipe(this.el.find('.Pipe2Lower'), 130, 40, 30, 6, this);
+		this.pipe3upper = new window.Pipe(this.el.find('.Pipe3Upper'), 150, 0, 30, 6, this);
+		this.pipe3lower = new window.Pipe(this.el.find('.Pipe3Lower'), 150, 50, 30, 6, this);
+		this.pipe4upper = new window.Pipe(this.el.find('.Pipe4Upper'), 180, 0, 15, 6, this);
+		this.pipe4lower = new window.Pipe(this.el.find('.Pipe4Lower'), 180, 35, 30, 6, this);
 		this.isPlaying = false;
 
 		var fontSize = Math.min(
@@ -40,10 +47,56 @@ window.Game = (function() {
 
 		// Update game entities.
 		this.player.onFrame(delta);
-		this.pipe.onFrame(delta);
+		this.pipe1upper.onFrame(delta);
+		this.pipe1lower.onFrame(delta);
+		this.pipe2upper.onFrame(delta);
+		this.pipe2lower.onFrame(delta);
+		this.pipe3upper.onFrame(delta);
+		this.pipe3lower.onFrame(delta);
+		this.pipe4upper.onFrame(delta);
+		this.pipe4lower.onFrame(delta);
+
+		this.checkCollisionWithPipes();
 
 		// Request next frame.
 		window.requestAnimationFrame(this.onFrame);
+	};
+
+	Game.prototype.checkCollisionWithPipes = function() {
+		var playerX = this.player.pos.x,
+			playerY = this.player.pos.y,
+			upipe1X = this.pipe1upper.pos.x,
+			upipe1Y = this.pipe1upper.pos.y,
+			lpipe1X = this.pipe1lower.pos.x,
+			lpipe1Y = this.pipe1lower.pos.y,
+			height1 = this.pipe1upper.height,
+			upipe2X = this.pipe2upper.pos.x,
+			upipe2Y = this.pipe2upper.pos.y,
+			lpipe2X = this.pipe2lower.pos.x,
+			lpipe2Y = this.pipe2lower.pos.y,
+			height2 = this.pipe2upper.height,
+			upipe3X = this.pipe3upper.pos.x,
+			upipe3Y = this.pipe3upper.pos.y,
+			lpipe3X = this.pipe3lower.pos.x,
+			lpipe3Y = this.pipe3lower.pos.y,
+			height3 = this.pipe3upper.height,
+			upipe4X = this.pipe4upper.pos.x,
+			upipe4Y = this.pipe4upper.pos.y,
+			lpipe4X = this.pipe4lower.pos.x,
+			lpipe4Y = this.pipe4lower.pos.y,
+			height4 = this.pipe4upper.height,
+			width   = this.pipe1upper.width;
+		//console.log('playerY: ' + playerY + ' lpipe1Y: ' + lpipe1Y + ' upipe1Y: ' + upipe1Y + ' height: ' + height);
+		if ((Math.abs(lpipe1X - playerX) < width / 2 && playerY + 5 > lpipe1Y) ||
+			(Math.abs(upipe1X - playerX) < width / 2 && playerY < upipe1Y + height1) ||
+			(Math.abs(lpipe2X - playerX) < width / 2 && playerY + 5 > lpipe2Y) ||
+			(Math.abs(upipe2X - playerX) < width / 2 && playerY < upipe2Y + height2) ||
+			(Math.abs(lpipe3X - playerX) < width / 2 && playerY + 5 > lpipe3Y) ||
+			(Math.abs(upipe3X - playerX) < width / 2 && playerY < upipe3Y + height3) ||
+			(Math.abs(lpipe4X - playerX) < width / 2 && playerY + 5 > lpipe4Y) ||
+			(Math.abs(upipe4X - playerX) < width / 2 && playerY < upipe4Y + height4)) {
+			this.gameover();
+		}
 	};
 
 	/**
@@ -63,7 +116,14 @@ window.Game = (function() {
 	 */
 	Game.prototype.reset = function() {
 		this.player.reset();
-		this.pipe.reset();
+		this.pipe1upper.reset();
+		this.pipe1lower.reset();
+		this.pipe2upper.reset();
+		this.pipe2lower.reset();
+		this.pipe3upper.reset();
+		this.pipe3lower.reset();
+		this.pipe4upper.reset();
+		this.pipe4lower.reset();
 	};
 
 	/**
--- app/scripts/pipe.js
+++ app/scripts/pipe.js
@@ -5,24 +5,28 @@ window.Pipe = (function() {
 	// All these constants are in em's, multiply by 10 pixels
 	// for 1024x576px canvas.
 	var SPEED = 10; // * 10 pixels per second
-	var INITIAL_POSITION_X = 60;
-	var INITIAL_POSITION_Y = 25;
 
-	var Pipe = function(el, game) {
+	var Pipe = function(el, initX, initY, height, width, game) {
 		this.el = el;
 		this.game = game;
-		this.pos = { x: 0, y: 0 };
+		this.pos = { x: initX, y: initY };
+		this.initPos = { x: initX, y: initY };
+		this.height = height;
+		this.width = width;
 	};
 
 	/**
 	 * Resets the state of the player for a new game.
 	 */
 	Pipe.prototype.reset = function() {
-		this.pos.x = INITIAL_POSITION_X;
-		this.pos.y = INITIAL_POSITION_Y;
+		this.pos.x = this.initPos.x;
+		this.pos.y = this.initPos.y;
 	};
 
 	Pipe.prototype.onFrame = function(delta) {
+		if (this.pos.x < -10) {
+			this.pos.x = this.initPos.x;
+		}
 		this.pos.x -= delta * SPEED;
 		// Update UI
 		this.el.css('transform', 'translateZ(0) translate(' + this.pos.x + 'em, ' + this.pos.y + 'em)');
